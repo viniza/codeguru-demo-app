99544f2d9fb453d206648203e876395e83c30ed7
Isidro Hernandez <isidrohg@viniza.tech>
2020-01-23 17:34:27 -0600

    y

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 
 cd /home/ec2-user/server
 #sudo mvn exec:java -Dexec.mainClass=com.company.sample.application.SalesSystem > /dev/null 2> /dev/null < /dev/null &
-sleep 5 > /dev/null 2> /dev/null < /dev/null &
+sleep 300 > /dev/null 2> /dev/null < /dev/null &
