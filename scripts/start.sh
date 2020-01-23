b734c4e2b223433c5d6e1d5dfd03286ed058614e
viniza <42075441+viniza@users.noreply.github.com>
2020-01-22 22:40:15 -0600

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/ec2-user/server
-sudo /usr/bin/java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo mvn exec:java -Dexec.mainClass=com.company.sample.application.SalesSystem
